c3b97548fb9770c75289364239a1c5e61aaefc45
Morten Kleveland <mortengkleveland@gmail.com>
2015-06-07 11:30:35 +0200

    Add Obj C project

---
+++ Hügge/Hügge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hügge
+//
+//  Created by Morten Kleveland on 07.06.15.
+//  Copyright (c) 2015 Morten G. Kleveland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hügge/Hügge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hügge
+//
+//  Created by Morten Kleveland on 07.06.15.
+//  Copyright (c) 2015 Morten G. Kleveland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
